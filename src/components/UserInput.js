089dced708a2114c12e28254b431a33b77abaad7
zhao-stanley <stanleyhzhao@gmail.com>
2023-03-07 23:15:59 -0500

    Update UserInput.js

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -3,7 +3,7 @@ export default function UserInput({ text, setText }) {
     <textarea
       value={text}
       onChange={(e) => setText(e.target.value)}
-      placeholder="Trip to the Bahamas, swam with some dolphins and drank from a coconut!"
+      placeholder="Trip to the Bahamas, swam with some dolphins, and drank from a coconut!"
       className="transition ease-linear w-full h-full min-h-[20vh] text-xs xl:text-sm placeholder:text-xs xl:placeholder:text-sm border-2 border-gray-200 focus-within:border-gray-900 outline-none rounded-lg p-2"
     />
   );
