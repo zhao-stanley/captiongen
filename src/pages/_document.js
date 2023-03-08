a669c467f8f03ed9c6cca862028189d43f905ae6
zhao-stanley <stanleyhzhao@gmail.com>
2023-03-07 22:08:03 -0500

    SEO

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,27 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>captiongen</title>
+        <meta name="title" content="captiongen" />
+        <meta
+          name="description"
+          content="Quickly generate the perfect caption for your social media post."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://captiongen.szhao.dev/" />
+        <meta property="og:title" content="captiongen" />
+        <meta
+          property="og:description"
+          content="Quickly generate the perfect caption for your social media post."
+        />
+        <meta property="og:image" content="/preview.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://captiongen.szhao.dev/" />
+        <meta property="twitter:title" content="captiongen" />
+        <meta
+          property="twitter:description"
+          content="Quickly generate the perfect caption for your social media post."
+        />
+        <meta property="twitter:image" content="/preview.png" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <body>
