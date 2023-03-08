f8b5b61d7e4db464b2823a891edb168c5bce7553
Stanley Zhao <stanleyhzhao@gmail.com>
2023-03-08 08:16:44 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
   const captionsRef = useRef(null);
 
   const prompt = `Generate 2 ${selectedStyle} Instagram captions, do not use hashtags, and do not use quotation marks. Clearly label them "1." and "2.". ${
-    selectedStyle === "Funny" && "Be funny and utilize jokes and common humor."
+    selectedStyle === "Funny" && "Be funny and humorous, utilize jokes."
   } ${
     selectedStyle === "Creative" &&
     "Be creative and clever, utilize puns and rhyme."
